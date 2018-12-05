274c50b71f4aed7cb9d8d710aef20e45bc498fc6
Tania Barone <tania.barone@anu.edu.au>
2018-12-06 10:19:55 +1100

    angular sky separation method added

--- source_code/Spectrum.py
+++ source_code/Spectrum.py
@@ -1,5 +1,7 @@
 from astropy.io import fits
+from astropy.coordinates import SkyCoord
 import numpy as np
+
 
 class Spectrum():
 	def __init__(self, filename):
@@ -7,13 +9,13 @@ class Spectrum():
 
 		with fits.open(self.filename) as hdu_list:
 		
-			self.ra   = hdu_list['PRIMARY'].header['RA'] 
-			self.dec  = hdu_list['PRIMARY'].header['DEC']
+			self.ra   = hdu_list['PRIMARY'].header['RA']  # degrees
+			self.dec  = hdu_list['PRIMARY'].header['DEC'] # degrees
 			
-			self.z    =  hdu_list[2].data['Z'][0] # redshift
+			self.z    =  hdu_list['SPALL'].data['Z'][0]   # redshift
 
 			flux      = hdu_list['COADD'].data['flux']
-			self.flux = flux / np.nanmedian(flux) # normalise
+			self.flux = flux / np.nanmedian(flux)         # normalise
 		
 			# wavelength in units of Angstroms	
 			self.wavelength = 10**hdu_list['COADD'].data['loglam']
@@ -21,9 +23,21 @@ class Spectrum():
 
 	@property
 	def rest_wavelength(self):
-		return(self.wavelength / (1 + self.z)) # de-redshift the wavelength array
+		''' de-redshift the wavelength array '''		
+		return(self.wavelength / (1 + self.z))
 
 
-	
+	def separation(self, s):
+		''' Return the angle on the sky between two objects '''
+		loc1 = SkyCoord(ra=self.ra, dec=self.dec, unit='deg')
+		loc2 = SkyCoord(ra=s.ra, dec=s.dec, unit='deg')
+
+		separation = loc1.separation(loc2)
+		return(separation)
+
+
+
+
+
 
 
