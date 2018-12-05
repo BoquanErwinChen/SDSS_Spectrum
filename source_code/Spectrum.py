fba93f9fd221c441e3f48d7fd9b2cb902d7fe55a
Tania Barone <tania.barone@anu.edu.au>
2018-12-06 09:47:14 +1100

    __init__ method written

--- source_code/Spectrum.py
+++ source_code/Spectrum.py
@@ -1,7 +1,21 @@
-
+from astropy.io import fits
+import numpy as np
 
 class Spectrum():
 	def __init__(self, filename):
 		self.filename = filename
 
+		with fits.open(self.filename) as hdu_list:
 		
+			self.ra   = hdu_list['PRIMARY'].header['RA'] 
+			self.dec  = hdu_list['PRIMARY'].header['DEC']
+			
+			self.z    =  hdu_list[2].data['Z'][0] # redshift
+
+			flux      = hdu_list['COADD'].data['flux']
+			self.flux = flux / np.nanmedian(flux) # normalise
+		
+			# wavelength in units of Angstroms	
+			self.wavelength = 10**hdu_list['COADD'].data['loglam']
+
+
