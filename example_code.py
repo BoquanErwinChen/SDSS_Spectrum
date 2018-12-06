1f8c72b2bc77baa15ffbc1e8720ed534c2224b88
Tania Barone <tania.barone@anu.edu.au>
2018-12-06 11:31:56 +1100

    wrote some example code

---
+++ example_code.py
@@ -0,0 +1,17 @@
+# Example code #
+
+from source_code.Spectrum import Spectrum
+
+s1 = Spectrum('spectra/spec-10000-57346-0002.fits')
+s2 = Spectrum('spectra/spec-6055-56102-0008.fits')
+
+print("Right ascension and Declination (degrees) \n" 
+	  "----------------------------------------  \n"    
+	  "{0:.2f}, {1:.2f} \n{2:.2f}, {2:.2f} \n".format(s1.ra, s1.dec,s2.ra, s2.dec))
+
+
+print("Seperation in arcseconds \n"
+	  "-----------------------  \n"
+	  "{0:.2f} arcsec".format(s1.separation(s2, 'arcsec')))
+
+
